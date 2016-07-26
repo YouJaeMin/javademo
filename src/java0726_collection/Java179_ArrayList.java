6e7908a012000e07c640e10416f3b1cbdd2ed311
YouJaeMin <wovkek@naver.com>
2016-07-26 12:03:48 +0900

    0726-3

--- src/java0726_collection/Java179_ArrayList.java
+++ src/java0726_collection/Java179_ArrayList.java
@@ -3,9 +3,8 @@ package java0726_collection;
 public class Java179_ArrayList {
 
 	public static void main(String[] args) {
-
-		System.out.println("aaaa");
-
+		
+		
 	}// end main()
 
 }// end class
